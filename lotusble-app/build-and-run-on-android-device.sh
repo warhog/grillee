1d4c9a02488351909e3954f911beab889b0a50b6
warhog <547310+warhog@users.noreply.github.com>
2020-08-22 08:34:21 +0200

    updated app versions

--- lotusble-app/build-and-run-on-android-device.sh
+++ lotusble-app/build-and-run-on-android-device.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # set the path to the platform tools
 PATH=$PATH:/home/$USER/Android/Sdk/platform-tools
 
-ionic cordova run android --device $1 $2 $3
+ionic cordova run android --device "$@"
 if [ $? -ne 0 ]; then
     echo "failed to run"
     exit 1
--- lotusble-app/build-release.sh
+++ lotusble-app/build-release.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/sh -e
 
-rm -f flt.apk
+rm -f lotusble.apk
 
 # set the path to the platform tools
 PATH=$PATH:/home/$USER/Android/Sdk/platform-tools
@@ -11,19 +11,18 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-
 # sign the jar file
-# jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore flt-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
-# if [ $? -ne 0 ]; then
-#     echo "failed to sign jar"
-#     exit 1
-# fi
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore lotusble-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
+if [ $? -ne 0 ]; then
+    echo "failed to sign jar"
+    exit 1
+fi
 
 # # run zipalignment
-# #/home/$USER/Android/Sdk/build-tools/28.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk flt.apk
-# /home/$USER/Android/Sdk/build-tools/28.0.0/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk flt.apk
-# if [ $? -ne 0 ]; then
-#     echo "failed to align zip file"
-#     exit 1
-# fi
+# #/home/$USER/Android/Sdk/build-tools/28.0.0/zipalign -v 4 platforms/android/build/outputs/apk/android-release-unsigned.apk lotusble.apk
+/home/$USER/Android/Sdk/build-tools/28.0.0/zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk lotusble.apk
+if [ $? -ne 0 ]; then
+    echo "failed to align zip file"
+    exit 1
+fi
 
